8d6d73bca46781a7b215479dd0d57674824bb1c2
roleyder <roggergarciadiaz@gmail.com>
2022-03-22 16:32:37 -0600

    :memo: "Update Documentation"
    update documentation

--- powershell-invoke-requests/invoke-post-method.ps1
+++ powershell-invoke-requests/invoke-post-method.ps1
@@ -1,2 +1,5 @@
 $postData = @{nombre='TestFromPowerShell';apellido='TestFromPowershell';telefono='TestFromPowershell'}
 Invoke-WebRequest -Method POST -Uri http://localhost:4001/api/contact -Body $postData -UseBasicParsing
+
+
+function prompt {"PS: Express Backend POST Usage >"}
