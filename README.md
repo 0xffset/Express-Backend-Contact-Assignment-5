6e8289b1c9ac9dc627513f10400558e67b02f702
roleyder <roggergarciadiaz@gmail.com>
2022-03-22 16:54:33 -0600

    :bento: "Add new assets"
    Add new assets

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 ## Doing POST requests
 ![Alt Text](/assets/invoke-post-gif.gif)
+
+## Doing GET requests
+![Alt Text](/assets/invoke-get-gif.gif)
--- powershell-invoke-requests/invoke-get-method.ps1
+++ powershell-invoke-requests/invoke-get-method.ps1
@@ -1,2 +1,4 @@
 $response = Invoke-WebRequest -Method GET -Uri http://localhost:4001/api/contact -UseBasicParsing
 ConvertFrom-Json $([String]::new($response.Content))
+
+function prompt {"PS: Express Backend GET Usage > "}
