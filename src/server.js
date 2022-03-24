229c399e2b37861d9bdf2053ecbd54f6e69b72f1
roleyder <roggergarciadiaz@gmail.com>
2022-03-23 19:30:59 -0600

    :label: Dynamically port
    dynamically port

--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
 const app = require('./express');
-const server = app.listen(4001, (err) => {
+const PORT = process.env.PORT || 5000
+const server = app.listen(PORT, (err) => {
     if (err) {
         console.log(err)
     }
-    console.info('Server started on port %s', 4001)
+    console.info('Server started on port %s', PORT)
 })
